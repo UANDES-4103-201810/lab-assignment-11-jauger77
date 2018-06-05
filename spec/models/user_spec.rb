a66c7fa3a49f431a7fd38e870860f3d305d67de1
jauger77 <jiauger@miuandes.cl>
2018-06-05 16:42:13 -0400

    last commit because of time

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,26 @@
 RSpec.describe User, type: :user do
   it "valid attributes to be valid" do
-    @user1 = create(:user)
+    @user1 = build(:user)
     expect(@user1).to be_valid
   end
+
+  it "valid username to be unique" do
+    @user1 = create(:user, user_name: "Jauger")
+    @user2 = build(:user)
+    expect(@user2.user_name).to be_valid
+  end
+
+  it "valid email to be unique" do
+    @user1 = create(:user, email: "jaimeignacio8@gmail.com")
+    @user2 = build(:user)
+    expect(@user2.email).to be_valid
+  end
+  
+  it "valid username lenth" do
+    @user1 = build(:user)
+    expect(@user1.user_name).to be_valid
+  
+
+
 end
+
